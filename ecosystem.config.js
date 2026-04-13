9379aec3fea588fcbb9b58053071a0e372a5c84b
Awan mabur <82402071+Awanmabur@users.noreply.github.com>
2026-04-13 14:07:42 +0300

    clean

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [{
-    name: 'jubarentals',
-    script: 'server.js',
-    instances: 1,
-    exec_mode: 'fork',
-    env: { NODE_ENV: 'production', PORT: 4000 }
-  }]
-};
